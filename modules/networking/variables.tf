de97ede727f20167862fa996f97169b846c78770
Gianluca Iumiento <giumiento@prostrategy.ie>
2025-11-24 19:12:35 +0000

    infracost key and environment

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -3,4 +3,7 @@ variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
 }
-
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
--- tests/networking/main.tf
+++ tests/networking/main.tf
@@ -29,4 +29,5 @@ module "networking" {
 
   #aws_region  = "us-east-1" 
   vpc_cidr    = "10.0.0.0/16"
+  environment = "dev"
 }
