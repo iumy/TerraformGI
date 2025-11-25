dc0e079863517cd0b98dbfbef14d983e4f77fa1d
Gianluca Iumiento <giumiento@prostrategy.ie>
2025-11-25 21:01:10 +0000

    eip, subnets, nat gateway and route tables

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -7,3 +7,17 @@ variable "environment" {
   description = "Environment name"
   type        = string
 }
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets (NAT Gateways)"
+  type        = list(string)
+}
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets (EKS nodes)"
+  type        = list(string)
+}
