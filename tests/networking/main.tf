3b29e27480ef2dd105ebea388fff626bf1417159
Gianluca Iumiento <giumiento@prostrategy.ie>
2025-11-25 21:48:36 +0000

    formatting

--- tests/networking/main.tf
+++ tests/networking/main.tf
@@ -33,7 +33,7 @@ module "networking" {
   #aws_region  = "us-east-1" 
   vpc_cidr             = "10.0.0.0/16"
   environment          = "dev"
-  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"] # NAT Gateways
+  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]   # NAT Gateways
   private_subnet_cidrs = ["10.0.10.0/24", "10.0.20.0/24"] # EKS Nodes
   availability_zones   = slice(data.aws_availability_zones.available.names, 0, 2)
 }
