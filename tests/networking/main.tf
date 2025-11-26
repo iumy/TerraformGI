b77d3c0bf7082752fbabac5f0779479089ce8beb
Gianluca Iumiento <giumiento@prostrategy.ie>
2025-11-26 09:06:17 +0000

    formatting

--- tests/networking/main.tf
+++ tests/networking/main.tf
@@ -16,7 +16,7 @@ terraform {
 }
 
 provider "aws" {
-  region                      = "us-east-1"
+  region = "us-east-1"
   access_key                  = "dummy"
   secret_key                  = "dummy"
   skip_credentials_validation = true
