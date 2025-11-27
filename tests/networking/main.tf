811648ae14914a34cd4a2b418163d1126041c852
Gianluca Iumiento <giumiento@prostrategy.ie>
2025-11-27 09:46:48 +0000

    infracost tagging recommendations

--- tests/networking/main.tf
+++ tests/networking/main.tf
@@ -17,6 +17,12 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+  default_tags {
+    tags = {
+      Environment = "Dev"
+      Service = "EKS_Cluster"
+    }
+  }
 }
 
 data "aws_availability_zones" "available" {
@@ -24,10 +30,9 @@ data "aws_availability_zones" "available" {
 }
 module "networking" {
   source = "../../modules/networking"
-
+  environment = "Dev"
   #aws_region  = "us-east-1" 
   vpc_cidr             = "10.0.0.0/16"
-  environment          = "dev"
   public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]   # NAT Gateways
   private_subnet_cidrs = ["10.0.10.0/24", "10.0.20.0/24"] # EKS Nodes
   availability_zones   = slice(data.aws_availability_zones.available.names, 0, 2)
