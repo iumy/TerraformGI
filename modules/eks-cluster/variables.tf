44bbcb7c3b669981fe7f724f0b1338955225095e
Gianluca Iumiento <giumiento@prostrategy.ie>
2025-11-27 21:36:49 +0000

    cluster version

--- modules/eks-cluster/variables.tf
+++ modules/eks-cluster/variables.tf
@@ -6,7 +6,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version for the cluster"
   type        = string
-  default     = "1.28"
+  default     = "1.34"
 }
 
 variable "cluster_role_arn" {
